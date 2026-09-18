194f011df9b95f1fb4d823977df3071537389037
ToJluK-V <sivakov_vanya@bk.ru>
2026-09-18 16:28:51 +0300

    homework 1 is done

---
+++ hw01/task3.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf("    *\n");
+	printf("   ***\n");
+	printf("  *****\n");
+	printf(" *******\n");
+	printf(" HHHHHHH\n" );
+	printf(" ZZZZZZZ\n");
+	return 0;
+}
+
