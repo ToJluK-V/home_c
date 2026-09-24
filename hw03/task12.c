ccc1be29843c0ac75b8843f53d6a7ec9f41037d8
ToJluK-V <sivakov_vanya@bk.ru>
2026-09-24 14:59:25 +0300

    Tasks 12, 13 are done

---
+++ hw03/task12.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	short int i;
+	//printf("Input *** number\n");
+	scanf("%hd",&i);
+	if(i<=99||i>999){
+		printf("Wrong number");
+		return 0;
+	}
+	short int ed = (i%10);
+	short int des = (i/10)%10;
+	short int sot =(i/100);
+	//short int sum = ed+des+sot;
+	printf("%hd", ed+des+sot);
+	
+	return 0;
+}
+
